0749d3fb0c71aa24be8d9a7f4fec15f495eb7993
aoyuzhang <haoyu.zhang@mail.mcgill.ca>
2021-05-19 07:26:01 -0700

    Added styling to button and created a loader page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import React, {useEffect, useState } from "react";
 import Weather from './components/Weather';
+import {Dimmer, Loader } from 'semantic-ui-react';
+
 
 
 function App() {
@@ -33,7 +35,11 @@ function App() {
       {(typeof data.main != 'undefined') ? (
         <Weather weatherData={data}/>
       ): (
-        <div></div>
+        <div>
+          <Dimmer active>
+            <Loader>Loading..</Loader>
+          </Dimmer>
+        </div>
       )}
 
     </div>

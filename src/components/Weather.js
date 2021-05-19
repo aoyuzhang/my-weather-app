bae1943ab07381cb23e61818ee1ade92a03aae41
aoyuzhang <haoyu.zhang@mail.mcgill.ca>
2021-05-19 07:00:16 -0700

    formated the time for sunrise and sunset, added today's date and humidity, added temperature sign

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import moment from 'moment';
 // import './styles.css';
 import{ Card } from 'semantic-ui-react'
 
@@ -6,10 +7,13 @@ const Weather = ({weatherData}) => (
   <Card>
     <Card.Content>
       <Card.Header className = "header">{weatherData.name} </Card.Header>
-      <p>Temprature: {weatherData.main.temp} </p>
-      <p>Sunrise: {weatherData.sys.sunrise}</p>
-      <p>Sunset: {weatherData.sys.sunset}</p>
-      <p>Description: {weatherData.weather[0].description}</p>
+      <p>Temprature: {weatherData.main.temp} &deg;C </p>
+      <p>Sunrise: {new Date(weatherData.sys.sunrise *1000).toLocaleTimeString('en-IN')} </p>
+      <p>Sunset: {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN')}</p>
+      <p>Description: {weatherData.weather[0].main}</p>
+      <p>Humidity: {weatherData.main.humidity} %</p>
+      <p>Day: {moment().format('dddd')}</p>
+      <p>Date: {moment().format('LL')}</p>
     </Card.Content>
   </Card>
 )
